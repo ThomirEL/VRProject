1444b49067d753125e56557a86d4de98f072aed5
gaia casotto <gaia.busacasotto@gmail.com>
2024-04-28 17:20:07 +0200

    working buttons
    GUN, Crown and matahari picture

---
+++ Assets/scripts/ToggleObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject model;
+    public void toggleActiveState(bool newState)
+    {
+        model.SetActive(newState);
+    }
+
+}
