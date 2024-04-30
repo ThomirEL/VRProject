e39dc0da632d2cdc3958ced0c51297e039cf77b7
gaia casotto <gaia.busacasotto@gmail.com>
2024-04-30 17:03:26 +0200

    room interactions
    couches, pillows and coffee mug

--- Assets/scripts/ToggleObject - Copy.cs
+++ Assets/scripts/ToggleObject - Copy.cs
@@ -7,11 +7,6 @@ public class ToggleObject : MonoBehaviour
     // Start is called before the first frame update
     public GameObject model;
 
-    public void Start()
-    {
-        model.gameObject.SetActive(false);
-    }
-
     public void toggleActiveState(bool newState)
     {
         model.SetActive(newState);
